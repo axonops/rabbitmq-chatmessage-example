43f77676bb7c7f8055b48b259f3aaaea5f8f1c90
Johnny Miller <johnny@digitalis.io>
2024-05-26 08:32:22 +0200

    feat: unique response queue with UUID in name and better error handling

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import logging
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import JSONResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
 import uuid
@@ -18,20 +18,85 @@ app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
 # RabbitMQ connection parameters
 rabbitmq_url = 'amqp://guest:guest@localhost:5672/%2f'
+request_queue_name = 'requests_queue'
 params = pika.URLParameters(rabbitmq_url)
-connection = pika.BlockingConnection(params)
-channel = connection.channel()
 
-# Declare the queues with TTL
-channel.queue_declare(queue='request_queue', durable=True, arguments={'x-message-ttl': 300000})
-channel.queue_declare(queue='response_queue', durable=True, arguments={'x-message-ttl': 300000})
+
+def create_response_queue(request_id: str):
+    try:
+        connection = pika.BlockingConnection(params)
+        channel = connection.channel()
+
+        # Generate a unique queue name using UUID
+        response_queue_name = generate_request_queue_name(request_id)
+
+        channel.queue_declare(queue=response_queue_name, durable=True, auto_delete=True,
+                              arguments={'x-expires': 600000})
+
+        logger.info(f"Declared queue {response_queue_name} with TTL of 10 minute and auto delete true")
+
+        # Close the connection
+        connection.close()
+
+        return response_queue_name
+
+    except Exception as e:
+        logger.error(f"Creation of response queue for request_id {request_id} @ {rabbitmq_url} failed: {e}")
+        raise HTTPException(status_code=500, detail=f"A problem has been encountered servicing request {request_id}")
+
+
+def generate_request_queue_name(request_id: str):
+    return f'response_queue_{request_id}'
+
+def send_message_to_request_queue(request_id: str, message):
+    try:
+        connection = pika.BlockingConnection(params)
+        channel = connection.channel()
+
+        channel.queue_declare(queue=request_queue_name, durable=True, arguments={'x-expires': 300000})
+
+        logger.info(f"Declared queue {request_queue_name} with message TTL of 5 minute")
+
+        channel.basic_publish(exchange='', routing_key=request_queue_name, body=json.dumps(message))
+
+        logger.info(f"Published message for request_id {request_id} to request queue {request_queue_name}")
+
+        # Close the connection
+        connection.close()
+
+    except Exception as e:
+        logger.error(f"Error encountered publishing message for request_id {request_id} to request queue {request_queue_name} @ {rabbitmq_url} : {e}")
+        raise HTTPException(status_code=503, detail=f"A problem has been sending request {request_id} for processing")
+
+
+def create_requests_queue():
+    try:
+        connection = pika.BlockingConnection(params)
+        channel = connection.channel()
+
+        channel.queue_declare(queue=request_queue_name, durable=True, arguments={'x-expires': 300000})
+
+        logger.info(f"Declared queue {request_queue_name} with message TTL of 5 minute")
+
+        # Close the connection
+        connection.close()
+
+    except Exception as e:
+        logger.error(f"Error encountered creating requests queue @ {rabbitmq_url} : {e}")
+        exit(-2)
+
+
+create_requests_queue()
 
 
 @app.post("/start")
 async def start_stream(text: str = Form(...)):
     request_id = str(uuid.uuid4())
     message = {"request_id": request_id, "text": text}
-    channel.basic_publish(exchange='', routing_key='request_queue', body=json.dumps(message))
+    response_queue_name = create_response_queue(request_id)
+    logger.info(f"Created unique response queue {response_queue_name} for request_id {request_id}")
+    logger.info("Sending message to request_queue")
+    send_message_to_request_queue(request_id, message)
     logger.info(f"Sent message to request_queue: {message}")
     return JSONResponse(content={"request_id": request_id})
 
@@ -39,17 +104,35 @@ async def start_stream(text: str = Form(...)):
 @app.get("/stream/{request_id}")
 async def stream_response(request_id: str):
     async def event_generator():
+        try:
+            connection = pika.BlockingConnection(params)
+            channel = connection.channel()
+        except Exception as e:
+            logger.error(f"Failed to connect to RabbitMQ  @ {rabbitmq_url}: {e}")
+            raise HTTPException(status_code=503, detail="Service Unavailable: Unable to connect to RabbitMQ")
+
+        response_queue_name = generate_request_queue_name(request_id)
+
+        logger.info(f"Starting listening on response_queue: {response_queue_name} for request_id {request_id}")
+
         while True:
-            method_frame, header_frame, body = channel.basic_get(queue='response_queue')
-            if method_frame:
-                response = json.loads(body)
-                if response['request_id'] == request_id:
-                    logger.info(f"Received message from response_queue: {response}")
-                    yield f"data: {response['text']}\n\n"
-                    channel.basic_ack(method_frame.delivery_tag)
-                    if response['text'] == "END":
-                        yield f"event: end\ndata: Stream ended\n\n"
-                        break
+            try:
+                method_frame, header_frame, body = channel.basic_get(queue=response_queue_name)
+                if method_frame:
+                    response = json.loads(body)
+                    if response['request_id'] == request_id:
+                        logger.info(f"Received message from response_queue: {response}")
+                        yield f"data: {response['text']}\n\n"
+                        channel.basic_ack(method_frame.delivery_tag)
+                        if response['text'] == "END":
+                            logger.info(f"Received end of stream message from response_queue: {response_queue_name}. Closing connection")
+                            connection.close()
+                            yield f"event: end\ndata: Stream ended\n\n"
+                            break
+            except Exception as e:
+                logger.error(f"An error occurred listening for response on response_queue_name {response_queue_name} @ {rabbitmq_url} : {e}")
+                raise HTTPException(status_code=503, detail="Internal Server Error")
+
             await asyncio.sleep(0.1)
 
     return StreamingResponse(event_generator(), media_type="text/event-stream")
--- producer.py
+++ producer.py
@@ -10,33 +10,49 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(level
 logger = logging.getLogger(__name__)
 
 rabbitmq_url = 'amqp://guest:guest@localhost:5672/%2f'
+request_queue_name = 'requests_queue'
 
 params = pika.URLParameters(rabbitmq_url)
-connection = pika.BlockingConnection(params)
-channel = connection.channel()
+connection = None
+channel = None
 
-channel.queue_declare(queue='request_queue', durable=True, arguments={'x-message-ttl': 300000})
-channel.queue_declare(queue='response_queue', durable=True, arguments={'x-message-ttl': 300000})
+try:
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel()
 
-def generate_random_text():
-    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=20))
+    channel.queue_declare(queue=request_queue_name, durable=True, arguments={'x-expires': 300000})
+
+    logger.info(f"Declared queue {request_queue_name} with message TTL of 5 minute")
+
+except Exception as e:
+    logger.error(f"Error encountered creating connection @ {rabbitmq_url}")
+    exit(-1)
+
+
+def generate_response_queue_name(request_id: str):
+    return f'response_queue_{request_id}'
 
 def callback(ch, method, properties, body):
     request = json.loads(body)
     request_id = request['request_id']
+    response_queue_name = generate_response_queue_name(request_id)
+    channel.queue_declare(queue=response_queue_name, durable=True, auto_delete=True,
+                          arguments={'x-expires': 600000})
+
     text = request['text']
     logger.info(f"Processing request: {request}")
     for word in text.split():
         response = {"request_id": request_id, "text": word}
-        channel.basic_publish(exchange='', routing_key='response_queue', body=json.dumps(response))
-        logger.info(f"Sent word to response_queue: {response}")
+        channel.basic_publish(exchange='', routing_key=response_queue_name, body=json.dumps(response))
+        logger.info(f"Sent word to response_queue {response_queue_name} : {response}")
         time.sleep(0.1)
     # Send an "END" message to signal the end of the stream
+    logger.info(f"Sending end message tag as finished")
     end_message = {"request_id": request_id, "text": "END"}
-    channel.basic_publish(exchange='', routing_key='response_queue', body=json.dumps(end_message))
+    channel.basic_publish(exchange='', routing_key=response_queue_name, body=json.dumps(end_message))
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
-channel.basic_consume(queue='request_queue', on_message_callback=callback)
+channel.basic_consume(queue=request_queue_name, on_message_callback=callback)
 
-logger.info('Waiting for messages. To exit press CTRL+C')
+logger.info(f'Waiting for messages on {request_queue_name}. To exit press CTRL+C')
 channel.start_consuming()
